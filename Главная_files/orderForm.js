b54eaa0a53701c4e6880dc6c8812a64cf0f444a9
Aleksarg <96260912+Aleksarg@users.noreply.github.com>
2021-12-29 01:49:52 +0500

    Add files via upload

---
+++ Главная_files/orderForm.js
@@ -0,0 +1,8 @@
+"use strict";!function(){var n=function(n){n.preventDefault(),n.stopPropagation();var o=this.dataset.popupId;o&&"function"==typeof window.requireFullConfOnce&&window.requireFullConfOnce(function(){window.require(["orderForm"],function(n){return n.open(o,"")})})},o=function(){var o=document.querySelectorAll(".wysiwyg__popup-link");o&&o.forEach(function(o){o.addEventListener("click",n)})};window.onload=function(){o()}}();
+//# sourceMappingURL=orderForm.js.map
+
+//# sourceMappingURL=orderForm.js.map
+
+//# sourceMappingURL=orderForm.js.map
+
+//# sourceMappingURL=orderForm.js.map
