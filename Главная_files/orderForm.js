2bc3bc717a0a7bc8eb55a5d763219851a87511ec
Aleksarg <96260912+Aleksarg@users.noreply.github.com>
2021-12-29 02:10:11 +0500

    Delete Главная_files directory

--- Главная_files/orderForm.js
+++
@@ -1,8 +0,0 @@
-"use strict";!function(){var n=function(n){n.preventDefault(),n.stopPropagation();var o=this.dataset.popupId;o&&"function"==typeof window.requireFullConfOnce&&window.requireFullConfOnce(function(){window.require(["orderForm"],function(n){return n.open(o,"")})})},o=function(){var o=document.querySelectorAll(".wysiwyg__popup-link");o&&o.forEach(function(o){o.addEventListener("click",n)})};window.onload=function(){o()}}();
-//# sourceMappingURL=orderForm.js.map
-
-//# sourceMappingURL=orderForm.js.map
-
-//# sourceMappingURL=orderForm.js.map
-
-//# sourceMappingURL=orderForm.js.map
